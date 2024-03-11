1e71ae9a6abaf0a97b2b0051871110c752b89aa5
Haria.Informatica <haria.informatica@gmail.com>
2024-03-11 13:01:48 +0000

    añadido mainherencia

---
+++ mainherencia.py
@@ -0,0 +1,13 @@
+from guagua import Guagua
+
+def main():
+    intercity = Guagua(asientos=50, ruedas=6)
+    lanzaroteBus = Guagua(asientos=80, ruedas=8)
+
+    intercity.desplazar(30)
+    lanzaroteBus.desplazar(40, 60)
+
+    intercity.informacion()
+    lanzaroteBus.informacion()
+
+main()
